df0502567508d73e908949eb91d51bd0a3053cd3
RMRattray <rmr3141@gmail.com>
2025-07-17 19:11:56 -0400

    Now *correctly* linking to SQLite

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -6,6 +6,13 @@ int main(int argc, char **argv)
     sqlite3 *db;
     char *zErrMsg = 0;
     int rc;
+
+    rc = sqlite3_open("SCRAPE.db", &db);
+    if( rc ){
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      sqlite3_close(db);
+      return(1);
+    }
     std::cout << "This application is the server for the Millennium Encryption system!" << std::endl;
     return 0;
 }
