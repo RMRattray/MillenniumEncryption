faa04a38dee29374472fd06c5fcfa20cc883bff5
RMRattray <rmr3141@gmail.com>
2025-07-17 18:26:33 -0400

    Link the SQLite library for use in the server

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <sqlite3.h>
 
 int main(int argc, char **argv)
 {
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
     std::cout << "This application is the server for the Millennium Encryption system!" << std::endl;
     return 0;
 }
